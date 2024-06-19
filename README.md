355a2712b832f57647d396157da553adc22825d4
Anton Schösser <anton.schoesser@tu-dresden.de>
2024-06-19 13:14:02 +0200

    added script and utils for pathloss maps, extended readme on this

--- README.md
+++ README.md
@@ -12,4 +12,19 @@ The scenario for ray tracing is created using Blender and a Python script. The s
 
 Adjustments to the scenario can be specified in the file `blender-python\conf\scene_attributes.yaml`. Please note, that the properties `transmitters`, `jammers` and `base_stations` are only used for visualization purposes.
 
-To visualize the scenario, the script `create_scenario_visualization.py` can be used. The script runs creates the scenario and visualizes in addition the radio devices. After the script has run, select the camera and click with the right mouse button on the camera. Select `Set camera active` and afterwards press `F12`to render the scene. For rendering, `Eevee` provides fast results, whereas the results with `Cycles` are more realistic. When finished, the file needs to be saved manually.
+To visualize the scenario, the script `create_scenario_visualization.py` can be used. The script runs creates the scenario and visualizes in addition the radio devices. After the script has run, select the camera and click with the right mouse button on the camera. Select `Set camera active` and afterwards press `F12` to render the scene. For rendering, `Eevee` provides fast results, whereas the results with `Cycles` are more realistic. When finished, the file needs to be saved manually.
+
+Please note, that the texture files are not part of this repository. They are only needed for visualization purpose, but not for the ray tracing. If needed, please download
+them:
+* [Concrete](https://www.poliigon.com/texture/poured-concrete-floor-texture/7656)
+* [Metal](https://www.poliigon.com/texture/lightly-worn-galvanised-steel-industrial-metal-texture/3129)
+
+and insert the files in the folders `blender-python\textures\ConcretePoured` and `blender-python\textures\MetalGalvanizedSteelWorn`, respectively.
+
+## Dataset Generation
+
+Generating the dataset to process consists of three steps. In a first step, a huge number of path loss maps for random transmitter locations are generated using ray tracing. Secondly, according to the number of legitimate transmitters and jammers and their associated transmit powers, they are combined (the powers are added up) to obtain the physical twin (PT) radio maps. In a third step, for each sample created in the last step, for each legitimate transmitter, the original locations of the legitimate transmitters are slightly assigned with a random offset to mimic localization inaccuracy and new pathloss maps are generated. The new resulting pathloss maps are then combined to obtain the digital twin (DT) radio maps. 
+
+### Pathloss Map Generation
+
+They are generated using the script `src\dataset_generation\pathloss_map_generation.py'`. The configuration is contained in the file `src\dataset_generation\conf\pathloss_map_generation.yaml`. Here, for example the scene number and the number of generated pathloss maps as well as the ray tracing parameters can be configured.
---
+++ src/utils/pl_utils.py
@@ -0,0 +1,211 @@
+''' Contains some utilities to work with path loss maps.'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import seaborn as sns
+import sionna.rt as srt
+import tensorflow as tf
+import yaml 
+
+repo_name = 'advancing-spectrum-anomaly-detection'
+module_path = __file__[:__file__.find(repo_name)+len(repo_name)]
+sys.path.append(os.path.abspath(module_path))
+
+
+# Color constants
+COLOR_TX_ORIG = 'green'
+
+
+class PathLossMap:
+    '''Class to save a path loss map.
+    
+    Contains the position of the transmitter and the path loss map.'''
+
+
+    def __init__(self, tx_pos, pathloss):
+        '''Initializes the PathLossMap object.
+
+        tx_pos : tuple
+            Position of the transmitter in meters.
+        pathloss : numpy.ndarray
+            Path loss map in dB.
+        '''
+        self.tx_pos = tx_pos
+        self.pathloss = pathloss
+
+    def show_pathloss_map(self, show_tx_pos=True):
+        """Shows the path loss map.
+        
+        show_tx_pos : bool
+            If True, the position of the transmitter is shown in the plot.
+        """
+
+        vmax = np.max(self.pathloss[np.isfinite(self.pathloss)]) # set biggest value that does not equal inf as vmax
+
+        sns.heatmap(self.pathloss.T, square=True, vmax=vmax, cbar=True, cbar_kws={'label': 'Path loss [dB]'}) 
+        if show_tx_pos:
+            plt.plot(self.tx_pos[0], self.tx_pos[1], 'v', color=COLOR_TX_ORIG, markersize=10, label='Transmitter')
+        plt.xlabel('x [m]')
+        plt.ylabel('y [m]')
+        plt.legend()
+        plt.gca().invert_yaxis()
+        plt.show()
+
+
+class PathLossMapCollection:
+    '''Class to save the results of the path loss map generation.
+
+    Contains the configuration and an array of PathLossMap objects.'''
+    def __init__(self, config):
+        self.config = config
+        self.pathlossmaps = []
+
+    def pathlossmap_for_tx_pos(self, tx_pos):
+        '''Returns the path loss map for a given transmitter position.
+        
+        tx_pos : tuple
+            Position of the transmitter in meters.
+        '''
+        for plm in self.pathlossmaps:
+            if np.array_equal(plm.tx_pos, tx_pos):
+                return plm.pathloss
+
+        raise ValueError('No path loss map found for the given transmitter position.')
+
+
+
+def smooth_path_loss(path_loss, n=5):
+    '''Sort out n biggest values and replace them with the n-th biggest value of the path loss map
+    and returns the smoothed path loss map.
+
+    In some earlier sionna version, extremely small values for the path loss in proximity to the 
+    transmitter were generated. This function serves to cope with this problem.
+    
+    path_loss : numpy.ndarray
+        Path loss map in dB.
+    n : int
+        Number of values to be sorted out.
+    '''
+
+    pl_threshold = np.sort(path_loss.flatten())[-n]
+    path_loss[path_loss >= pl_threshold] = pl_threshold
+    return path_loss
+
+
+def create_sionna_sample(config, scene, scene_nr, tx_pos=None, num_rays=int(1e6)):
+    """Returns one path loss map object using Sionna.
+    
+    config : dict
+        Configuration parameters.
+    scene : sionna.rt.Scene
+        Scene object.
+    scene_nr : int
+        index of the current scene.
+    tx_pos : tuple
+        "2D Position of the transmitter in meters.
+        If None, the transmitter position is randomly chosen.
+    num_rays : int
+        Nubmer of rays to be launched to compute the coverage map.
+    """
+    
+    scene_size = scene.size.numpy()
+
+    if tx_pos == None:
+         while(True):
+             tx_pos = [np.random.uniform(0, scene_size[0]),
+                       np.random.uniform(0, scene_size[1]),
+                       config['tx_height']]
+             if enclosed_in_obstacle(tx_pos,scene_nr,round_int=False):
+                 continue
+             else:
+                 break
+    elif enclosed_in_obstacle(tx_pos,scene_nr,round_int=False):
+        raise ValueError('Given transmitter position is enclosed within an obstacle.')
+            
+    tx = srt.Transmitter(name=f'tx', position=tx_pos)
+    scene.add(tx)
+
+    # create coverage map
+    cm = scene.coverage_map(max_depth=config['cm_max_depth'],
+                            cm_center=tf.cast([scene_size[0]/2, scene_size[1]/2, config['rx_height']], dtype=tf.float32),
+                            cm_orientation=tf.cast([0,0,0], dtype=tf.float32),
+                            cm_size=scene.size[:2],                                       # CM only 2D, whereas scene has a 3D size
+                            cm_cell_size=(config['cell_size'], config['cell_size']),      # Grid size of coverage map cells in m
+                            diffraction=config['diffraction'],
+                            edge_diffraction=config['edge_diffraction'],
+                            num_samples=config['num_samples'])
+    
+    scene.remove(tx.name)
+
+    path_loss_db = -10 * np.log10(cm.as_tensor().numpy()[0].T) # minus to convert gain to loss, transpose as otherwise x and y are swapped
+    plm = PathLossMap(tx_pos, path_loss_db)
+    
+    return plm
+
+
+def enclosed_in_obstacle(tx_pos, scene_nr, round_int=False):
+    """Checks if the transmitter is enclosed within any obstacle(s).
+
+    tx_pos : list (int)
+        list containing x,y and z co-ordinates of the transmitter.
+    scene_nr : int
+        index of the current scene.
+    """
+
+    if round_int:
+        tx_pos[:-1] = np.round(tx_pos[:-1])
+
+    # Only read the scene description if it has not been read before
+    if not hasattr(enclosed_in_obstacle, '_obstacles'):
+        path = os.path.join(os.path.dirname(__file__),'..','..','scenes',f'scene{scene_nr}','scene_attributes.yaml')
+    
+        # Load the YAML file
+        with open(path, 'r') as file:
+            scene_description = yaml.safe_load(file)
+        obstacles = scene_description.get('obstacles', {})
+
+        # Attach the obstacles to the function
+        enclosed_in_obstacle._obstacles = obstacles
+
+    if enclosed_in_obstacle._obstacles == {}:     # No obstacles
+        return False
+
+    for obstacle in enclosed_in_obstacle._obstacles.values():
+        x, y, z = obstacle['anchor_point']
+        w, l, h = obstacle['edge_length']
+        if x<= tx_pos[0] <= (x+w) and y<= tx_pos[1] <= (y+l) and tx_pos[2] <= h:
+            return True
+        else:
+            continue
+    return False
+
+
+def get_obstacle_mask(scene_nr, scene_size):
+    """
+    Returns a boolean mask of the scene where obstacles are True
+    and free space is False.
+
+    Input
+    ------
+    scene_nr : int
+        Index of the current scene.
+    scene_size : tuple
+        Size of the scene in meters.
+
+    Output
+    ------
+    mask : np.ndarray
+        Boolean mask of the scene.
+    """
+
+    mask = np.zeros([scene_size[0], scene_size[1]], dtype=bool)
+
+    for i in range(scene_size[0]):
+        for j in range(scene_size[1]):
+            if enclosed_in_obstacle([i, j, 0], scene_nr):
+                mask[i, j] = True
+
+    return mask
+
